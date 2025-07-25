01ef1d0115031ee95bc50f068711510066bce564
Suchith Raj <suchithrajkurut@gmail.com>
2025-07-25 09:27:22 +0530

    add all initial files

--- file.py
+++ file.py
@@ -1,7 +1,15 @@
-def print_items(n):
-    for i in range(n):
-        for j in range(n):
-            print(i,j)
-        
-    for k in range(n):
-        print (k)
+
+def __init__(self,color):
+    self.color = color
+
+    def get_color(self,color):
+        return self.color
+    
+    def set_color(self,color):
+        self.color = color
+
+        Cookiesone = Cookies("blue")
+        cookietwo = Cookies("red")
+
+        print('cookie is green', Cookiesone.get_color())
+        print('cookie is red', cookietwo.set_color())   
