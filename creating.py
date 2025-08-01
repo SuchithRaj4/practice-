a742c36afaf0bc16bb13a2f7d7cbb8665f19e971
Suchith Raj <suchithrajkurut@gmail.com>
2025-08-01 13:40:00 +0530

    add all initial files

--- creating.py
+++ creating.py
@@ -32,3 +32,25 @@ def pop(self,value):
     if length == 0:
         self.head = None
         self.tail = None
+
+def preped(self,value):
+    new_node = Node(value)
+    if self.length == 0:
+        self.head = new_node
+        self.tail = new_node
+    else:
+        new_node.next = self.head
+        self.head = new_node
+        self.length = +1
+        return True
+
+def pop_first(self):
+    if self.length == 0:
+        return None
+    temp = self.head
+    self.head = self.head.next
+    temp.next = None
+    self.length = -1
+    return True
+
+    
--- linkedlist.py
+++ linkedlist.py
@@ -4,14 +4,26 @@ class Node:
         self.Next = None
 
 class LinkedList:
-    def prepend(self,value):
-        new_node = Node(value)
+    def get(self,index):
+      if index <0 or  index >= self.length:
+          return None
+      temp = self.head
+      for _ in range(index):
+          temp = temp.next
+          return temp
+      
+    def pop_first(self):
         if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
-            self.length = +1
-            return True 
-        
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length = -1
+        return True 
+    
+    def set_values(self,index,value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
